cb0eef5481baaa5e502031108f2caeb34b5ad4e1
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2026-06-14 02:12:49 +0530

    Initial commit

---
+++ src/main/java/com/paul/clauseiq/ClauseIqApplication.java
@@ -0,0 +1,13 @@
+package com.paul.clauseiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClauseIqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClauseIqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paul/clauseiq/ClauseIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul.clauseiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClauseIqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
